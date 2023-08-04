da6505360a0d7f691f2cfdde78a0d92a2bf0ba8f
notapie <notapie@yandex.ru>
2023-08-04 13:13:42 +0900

    feat: add overflow check in util functions

--- include/Util.hpp
+++ include/Util.hpp
@@ -1,25 +1,27 @@
 #pragma once
 
 #include <cstdint>
+#include <eosio/asset.hpp>
 #include <definitions/Definitions.hpp>
 
 int64_t GetRateOf(int64_t value, int64_t rate) {
     return static_cast<int64_t>(
-                   (static_cast<int128_t>(value) * static_cast<int128_t>(rate)) / DEFAULT_FEE_PRECISION
-           ) / 100;
+        (static_cast<int128_t>(value) * static_cast<int128_t>(rate)) / DEFAULT_FEE_PRECISION
+    ) / 100;
 }
 
 int64_t GetLiquidity(const int64_t in_amount, const int64_t supply, const int64_t pool) {
-    return static_cast<int64_t>((static_cast<uint128_t>(in_amount) * static_cast<uint128_t>(supply))
-                                / static_cast<uint128_t>(pool));
+    const uint128_t result = (static_cast<uint128_t>(in_amount) * static_cast<uint128_t>(supply))
+        / static_cast<uint128_t>(pool);
+    eosio::check(result <= static_cast<uint128_t>(eosio::asset::max_amount), "Transaction amount is too large");
+
+    return static_cast<int64_t>(result);
 }
 
 int64_t CalculateToPayAmount(const int64_t liquidity, const int64_t pool, const int64_t supply) {
-    return static_cast<int64_t>((static_cast<uint128_t>(liquidity) * static_cast<uint128_t>(pool))
-                                / static_cast<uint128_t>(supply));
+    return GetLiquidity(liquidity, pool, supply);
 }
 
 int64_t CalculateInAmount(const int64_t out_amount, const int64_t pool_in_amount, const int64_t pool_out_amount) {
-    return static_cast<int64_t>((static_cast<uint128_t>(out_amount) * static_cast<uint128_t>(pool_in_amount))
-                                / static_cast<uint128_t>(pool_out_amount));
+    return GetLiquidity(out_amount, pool_in_amount, pool_out_amount);
 }
